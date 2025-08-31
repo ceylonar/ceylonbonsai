5ccb5dc47131e0f9243062ac44f1a06084ca1405
Ceylon Ar <ceylonar.main@gmail.com>
2025-08-31 21:53:08 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyCtQj1DbvOemFL0l5QvuEGCHt-MxLOhgbw",
+  authDomain: "ceylon-bonsai-museum-landing.firebaseapp.com",
+  projectId: "ceylon-bonsai-museum-landing",
+  storageBucket: "ceylon-bonsai-museum-landing.firebasestorage.app",
+  messagingSenderId: "880933100478",
+  appId: "1:880933100478:web:b4661af84aed343349f7ef"
 };
 
 // Initialize Firebase
